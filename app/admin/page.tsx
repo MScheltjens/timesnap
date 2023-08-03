3cb0da2056e0a32d9ed415750bea4ff3d5fbbaa2
Mathias Scheltjens <mathias.scheltjens@gmail.com>
2023-08-03 22:26:24 +0200

    clean admin

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -6,10 +6,6 @@ import { cookies } from "next/headers";
 export const dynamic = "force-dynamic";
 
 export default async function Page() {
-  const supabase = createServerComponentClient<TDatabase>({ cookies });
-
-  getUser(supabase);
-
   return (
     <div>
       <h2>Page</h2>
