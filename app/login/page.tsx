feb5b1d4566968de96a6873d05591f066b49fd56
Mathias Scheltjens <mathias.scheltjens@gmail.com>
2023-08-03 17:13:08 +0200

    login supabase done

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,15 @@
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { TDatabase } from "@/lib/supabase.types";
 import Messages from "./messages";
 
-export default function Page() {
+export default async function Page() {
+  const supabase = createServerComponentClient<TDatabase>({ cookies });
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
   return (
     <div className="flex-1 flex w-full px-8 sm:max-w-md  items-center gap-2 mt-44 text-black">
       <form className="flex-1 flex flex-col justify-center gap-2 text-foreground" action="/auth/sign-in" method="post">
@@ -13,9 +22,6 @@ export default function Page() {
         </label>
         <input className="rounded-md px-4 py-2 bg-inherit border mb-6" type="password" name="password" placeholder="••••••••" required />
         <button className="bg-green-700 rounded px-4 py-2 text-white mb-2">Sign In</button>
-        <button formAction="/auth/sign-up" className="border border-gray-700 rounded px-4 py-2 text-white mb-2">
-          Sign Up
-        </button>
         <Messages />
       </form>
     </div>
