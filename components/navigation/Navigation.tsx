edb78823f661331648190298b48c0feb0893e765
Mathias Scheltjens <mathias.scheltjens@gmail.com>
2023-08-04 10:16:41 +0200

    home only visible on other pages

--- components/navigation/Navigation.tsx
+++ components/navigation/Navigation.tsx
@@ -1,12 +1,15 @@
 "use client";
 
+import { usePathname } from "next/navigation";
 import { NavItem } from "../navItem/NavItem";
 
 export const Navigation = () => {
+  const pathname = usePathname();
+
   return (
     <nav className="flex items-center text-xs sm:text-base ">
       <ul className="flex gap-2 lg:gap-6 flex-wrap justify-end">
-        <NavItem label="home" href="/" />
+        {pathname !== "/" && <NavItem label="home" href="/" />}
         <NavItem label="photography" href="photography" />
         <NavItem label="mixed art" href="mixed-art" />
         <NavItem label="contact" href="contact" />
