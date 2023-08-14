f9efde0ce7c50dc80012fe9c1e65584b2daf7ccb
Mathias Scheltjens <mathias.scheltjens@gmail.com>
2023-08-14 18:13:35 +0200

    generatgeparams out

--- app/[work]/[id]/page.tsx
+++ app/[work]/[id]/page.tsx
@@ -16,9 +16,9 @@ export default async function Page({ params }: { params: { work: string; id: str
     return notFound();
 }
 
-export async function generateStaticParams({ params }: { params: { work: string } }) {
-    const { get } = getNetwork();
-    const data = await get<TDBImg[]>(`/${params.work}?select=*`);
-    if (data) return data.map((item) => ({ id: String(item.id), work: params.work }));
-    return notFound();
-}
+// export async function generateStaticParams({ params }: { params: { work: string } }) {
+//     const { get } = getNetwork();
+//     const data = await get<TDBImg[]>(`/${params.work}?select=*`);
+//     if (data) return data.map((item) => ({ id: item.id, work: params.work }));
+//     return notFound();
+// }
