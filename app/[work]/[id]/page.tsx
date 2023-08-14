44335d4b0cc4e26d36b8ed0c78452a3a421da3bf
Mathias Scheltjens <mathias.scheltjens@gmail.com>
2023-08-14 16:09:12 +0200

    api handlers in comment

--- app/[work]/[id]/page.tsx
+++ app/[work]/[id]/page.tsx
@@ -1,13 +1,17 @@
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
-// import { getNetwork } from '@/http';
 import { TDBImg } from '@/types/types';
 
 export default async function Page({ params }: { params: { work: string; id: string } }) {
-    const supabase = createServerComponentClient<TDBImg[]>({ cookies });
-    const { data } = await supabase.from(params.work).select('*').eq('id', String(params.id));
+    const supabase = createServerComponentClient<TDBImg>({ cookies });
+    const { data: imageData } = (await supabase.from(params.work).select('*').eq('id', params.id)) as { data: TDBImg[] };
 
-    return <div className="mt-44">{/* <Image src={data[0].img_url!} alt="DFSQ" width={500} height={500} /> */}</div>;
+    return (
+        // <div className="mt-44">
+        //     <Image src={imageData.img_url!} alt="DFSQ" width={500} height={500} />
+        // </div>
+        <></>
+    );
 }
 
 export const dynamic = 'force-dynamic';
--- app/api/[work]/[id]/route.ts
+++ app/api/[work]/[id]/route.ts
@@ -1,13 +1,15 @@
-import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
-import { cookies } from 'next/headers';
-import { NextResponse } from 'next/server';
-import { TDBImg } from '@/types/types';
+// import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
+// import { cookies } from 'next/headers';
+// import { NextResponse } from 'next/server';
+// import { TDBImg } from '@/types/types';
 
-export async function GET(request: Request, { params }: { params: { work: string; id: string } }) {
-    const supabase = createRouteHandlerClient<TDBImg>({ cookies });
-    const { data } = (await supabase.from(params.work).select('*').eq('id', params.id)) as { data: TDBImg[] };
-    if (data) return new NextResponse(JSON.stringify(data[0]), { status: 200 });
-    return new NextResponse(JSON.stringify(data[0]), { status: 200 });
-}
+// export async function GET(request: Request, { params }: { params: { work: string; id: string } }) {
+//     const supabase = createRouteHandlerClient<TDBImg>({ cookies });
+//     const { data } = (await supabase.from(params.work).select('*').eq('id', params.id)) as { data: TDBImg[] };
+//     if (data) return new NextResponse(JSON.stringify(data[0]), { status: 200 });
+//     return new NextResponse(JSON.stringify(data[0]), { status: 200 });
+// }
 
-export const dynamic = 'force-dynamic';
+// export const dynamic = 'force-dynamic';
+
+export {};
