32b2342fd788c5f68f5926f5a3d57a7d436df987
Mathias Scheltjens <mathias.scheltjens@gmail.com>
2023-08-14 22:20:28 +0200

    admin route protected

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,6 +1,16 @@
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
 import { UploadForm } from '@/components';
 
-export default function Page() {
+export default async function Page() {
+    const supabase = createServerComponentClient({ cookies });
+    const {
+        data: { session },
+    } = await supabase.auth.getSession();
+
+    if (!session) redirect('/login');
+
     return (
         <div className="mt-36">
             <UploadForm />
