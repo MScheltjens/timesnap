d7c7d5f2b52f685493d2fe106a39a6b49f7c5a53
Mathias Scheltjens <mathias.scheltjens@gmail.com>
2023-08-29 19:52:53 +0200

    text update photography

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
                 id="photography"
                 nextId="mixed-art"
                 title="Photography"
-                text="lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur"
+                text="Voor mezelf en mijn fotografie te kaderen schreef ik het volgende neer : ''Het donkerste van de Geest wordt steeds gezien door het licht van de Ziel. Het voelt zich opgelucht en opgelicht om samen te werken voor een midden-grijs evenwicht. Want wat zijn gedachten als je ze gelooft, denken absoluut geen nood, en je hart is even groot? Dan stopt het dromen bij jezelf, geen zwart plafond of witte vloer maar één met alles en alles meteen."
             />
             <Hero
                 image={mixedArt}
